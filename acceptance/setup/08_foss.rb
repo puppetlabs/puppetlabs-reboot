5ba99c6e6df3aa553cb09201fabd2681640ed6de
Ryan Gard <ryan.gard@puppetlabs.com>
2013-12-10 11:23:16 -0800

    Force the foss.rb pre-suite to execute after Puppet pre-suite.

--- acceptance/setup/08_foss.rb
+++ acceptance/setup/08_foss.rb
@@ -1,8 +1,7 @@
 test_name "Install Reboot and Registry Module on Master" do
-  on master, "yum install git -y"
   on master, "git clone https://github.com/joshcooper/puppetlabs-reboot /etc/puppet/modules/reboot"
   on master, "git clone https://github.com/puppetlabs/puppetlabs-registry /etc/puppet/modules/registry"
-  
+
   with_puppet_running_on master, :main => { :verbose => true, :daemonize => true } do
     on agents, puppet("plugin download --server #{master}")
   end
