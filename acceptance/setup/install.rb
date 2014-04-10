682341ba530d9bacf55af7d12f15dc93c758c563
Travis Fields <travis@puppetlabs.com>
2014-04-10 12:03:17 -0700

    Added Puppet Enterprise master install of registry module
     - Previous code was doing a git pull from the registry modules master
       branch on GitHub.  We now ensure the module comes from a stable Forge
       version instead, that is installed on the PE master.

--- acceptance/setup/install.rb
+++ acceptance/setup/install.rb
@@ -1,3 +1,5 @@
 test_name "Installing Puppet Enterprise" do
   install_pe
+  step "Install required registry module"
+  on master, puppet('module install puppetlabs/registry --modulepath /opt/puppet/share/puppet/modules')
 end
