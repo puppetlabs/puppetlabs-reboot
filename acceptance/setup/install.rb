f4b89c76202d8c0f25c81bdd57ccd68270b52a16
Travis Fields <travis@puppetlabs.com>
2014-04-14 11:21:03 -0700

    add a reusable config for PE testing config

--- acceptance/setup/install.rb
+++ acceptance/setup/install.rb
@@ -1,5 +1,4 @@
 test_name "Installing Puppet Enterprise" do
   install_pe
-  step "Install required registry module"
   on master, puppet('module install puppetlabs/registry --modulepath /opt/puppet/share/puppet/modules')
 end
