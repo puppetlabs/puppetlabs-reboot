0eaa1d442e5167670976de52e7370fa02f10897c
Travis Fields <travis@puppetlabs.com>
2014-08-21 17:13:45 -0700

    Add stdlib to git installed modules

--- acceptance/setup/install.rb
+++ acceptance/setup/install.rb
@@ -10,13 +10,11 @@ test_name "Installing Puppet Enterprise" do
       on host, "mkdir -p #{host['distmoduledir']}/reboot"
       result = on host, "echo #{host['distmoduledir']}/reboot"
       target = result.raw_output.chomp
-    else
-      target = "#{host['distmoduledir']}/reboot"
-      install_package host, 'git'
+      %w(lib manifests metadata.json).each do |file|
+        scp_to host, "#{proj_root}/#{file}", "#{target}"
+      end
+      on host, "cd #{host['distmoduledir']} && git clone --branch 1.0.x --depth 1 git://github.com/puppetlabs/puppetlabs-registry.git registry"
+      on host, "cd #{host['distmoduledir']} && git clone --branch 4.3.2 --depth 1 git://github.com/puppetlabs/puppetlabs-stdlib.git stdlib"
     end
-    %w(lib manifests metadata.json).each do |file|
-      scp_to host, "#{proj_root}/#{file}", "#{target}"
-    end
-    on host, "cd #{host['distmoduledir']} && git clone --branch 1.0.x --depth 1 git://github.com/puppetlabs/puppetlabs-registry.git registry"
   end
 end
