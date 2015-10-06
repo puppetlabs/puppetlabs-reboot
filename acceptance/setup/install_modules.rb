cba05187da350b0a277672fb017429a81c1ca765
Iristyle <Iristyle@github>
2015-10-06 13:49:34 -0700

    (MODULES-2674) Update module install + ca certs
     - In acceptance pre-suite, git clone was previously used to install
       modules, but this is no longer necessary given a pre-suite step that
       installs windows certs for PMT

--- acceptance/setup/install_modules.rb
+++ acceptance/setup/install_modules.rb
@@ -1,15 +1,10 @@
 test_name 'Install modules' do
 
   hosts.each do |host|
-    on host, "mkdir -p #{host['distmoduledir']}"
-    if host['platform'] =~ /windows/
-      on host, "cd #{host['distmoduledir']} && git clone --branch 1.1.x --depth 1 git://github.com/puppetlabs/puppetlabs-registry.git registry"
-      on host, "cd #{host['distmoduledir']} && git clone --branch 4.6.x --depth 1 git://github.com/puppetlabs/puppetlabs-stdlib.git stdlib"
-    else
-      on host, 'rm -rf /etc/puppetlabs/puppet/environments/production/modules/reboot'
-      on host, puppet('module install puppetlabs/stdlib')
-      on host, puppet('module install puppetlabs/registry')
-    end
+    step 'Install Reboot Module Dependencies'
+    on(host, puppet('module install puppetlabs-stdlib'))
+    on(host, puppet('module install puppetlabs-registry'))
+
     copy_root_module_to(host, :module_name => 'reboot')
   end
 end
