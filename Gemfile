4776f22bcb9c11eb9516dc1a95c1975332eaea8c
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-10-07 13:18:01 +0800

    (MODULES-8046) Added tests for `reboot::wait`
    Note that the whitespace changes in the system_tests gemfile are due the PDK
    update process finding a change and fixing it.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ group :development do
   gem "puppet-module-posix-dev-r#{minor_version}",     require: false, platforms: [:ruby]
   gem "puppet-module-win-default-r#{minor_version}",   require: false, platforms: [:mswin, :mingw, :x64_mingw]
   gem "puppet-module-win-dev-r#{minor_version}",       require: false, platforms: [:mswin, :mingw, :x64_mingw]
+  gem "bolt",                                          require: false if Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new('2.3.0')
 end
 group :system_tests do
   gem "puppet-module-posix-system-r#{minor_version}", require: false, platforms: [:ruby]
--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -3,3 +3,28 @@
 class Object
   alias must should
 end
+
+# Bolt may not available e.g. using an old version of ruby.
+# Therefore we should safely attempt to load bolt and expose
+# a method which can be used in tests to change behavior e.g.
+# skip bolt tests if bolt is not loaded.
+begin
+  # Bolt prior to 1.0 had issues with localization therefore we need
+  # Bolt 1.0 and above. Bolt 1.0 requires Puppet 6.0.0 and above. We
+  # can't express this in the Gemfile so we need to guard loading here.
+  # Only attempt to load bolt if the Puppet version constraint is met
+  if Gem::Version.new(Puppet.version) >= Gem::Version.new('6.0.0')
+    require 'bolt/executor'
+    require 'bolt/target'
+  end
+rescue LoadError # rubocop:disable Lint/HandleExceptions
+end
+
+def bolt_loaded?
+  !defined?(Bolt).nil?
+end
+
+def tasks_available?
+  # Tasks (--tasks) were introduced in Puppet Agent 5.4.0 (PUP-7898)
+  Gem::Version.new(Puppet.version) >= Gem::Version.new('5.4.0')
+end
