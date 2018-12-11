f30c43f7f65eed22f82b6c3acefdd1c355f65538
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-12-11 11:48:31 -0600

    (MODULES-8353) Infinite reboot wait loop bug
    This resolves an issue where the precision for last boot can cause you to get stuck in an infinite reboot loop if the machine reboots quickly within the same minute.  Go here for more info https://tickets.puppetlabs.com/browse/MODULES-8353

--- tasks/last_boot_time_win.ps1
+++ tasks/last_boot_time_win.ps1
@@ -1,3 +1,3 @@
 $boot = Get-WmiObject -Class Win32_OperatingSystem
 $dt = $boot.ConvertToDateTime($boot.LastBootUpTime)
-Write-Output "$($dt.ToShortDateString()) $($dt.ToShortTimeString())"
+Write-Output "$($dt.ToShortDateString()) $($dt.ToLongTimeString())"
